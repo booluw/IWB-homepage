b7300dc467b66146a94d28493b0dd157c52e7265
= <=>
2022-05-08 12:01:02 +0100

    Desktop done.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,248 @@
 <template>
-  <Tutorial />
+  <section>
+    <section class="hero">
+      <h1 class="hero__heading">
+        International leadership bootcamp
+      </h1>
+      <p class="hero__paragraph">
+        We are at the frontline of business development, career and leadership growth, digital technologies and everything in-between.
+      </p>
+      <button class="btn hero__btn">
+        Register now
+      </button>
+    </section>
+    <section class="section">
+      <div class="row m-0">
+        <div class="col-md-6">
+          <h2 class="section__heading">
+            our mantra
+          </h2>
+          <p class="section__paragraph">
+            We are at the frontline of business development, career and leadership growth, digital technologies and everything in-between.
+            <b>Ideas Worth Billions</b> is geared towards changing Africa’s narrative. Creating Value, Sustainable solutions and exposing young
+            Africans to global opportunities.
+          </p>
+          <br>
+          <p class="section__paragraph">
+            We are brooding a thriving community of Africa’s finest thought leaders, disruptors, creators, influencers etc, who are combating
+            extreme poverty and underdevelopment.  Our programs are well tailored to re-create the African narrative.
+            Our Workstream consists expert across the globe.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section id="programs" class="section">
+      <div class="row m-0 justify-content-center">
+        <div class="col-md-3">
+          <h3 class="section__heading--micro">
+            connecting economies
+          </h3>
+        </div>
+        <div class="col-md-3">
+          <h3 class="section__heading--micro">
+            creating solutions
+          </h3>
+        </div>
+        <div class="col-md-3">
+          <h3 class="section__heading--micro">
+            exploring technologies
+          </h3>
+        </div>
+        <div class="col-md-3">
+          <h3 class="section__heading--micro">
+            discussing opportunities
+          </h3>
+        </div>
+      </div>
+      <div class="row m-0">
+        <div class="col-md-6 p-0">
+          <h2 class="section__heading">
+            our programs
+          </h2>
+        </div>
+      </div>
+      <div class="row m-0">
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/Ci20.png" alt="Image of two attendants smiling at #Ci20 2020" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                #Ci20
+              </h3>
+              <h4 class="card__heading--micro">
+                Connecting Ideas to Opportunities
+              </h4>
+              <p class="card__paragraph">
+                Bringing together students from various campuses, exposing them to digital opportunities and how they can monetize them.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/CiF.png" alt="A lady on purple hijab looking into empty space" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                #CiF
+              </h3>
+              <h4 class="card__heading--micro">
+                Campus Influencers Fellowship
+              </h4>
+              <p class="card__paragraph">
+                Six weeks intensive mentoring on Digital Monetization Business Development and Leadership.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/campus-storm.png" alt="Image of a lady holding a microphone" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                #KBS
+              </h3>
+              <h4 class="card__heading--micro">
+                Kingdom Business School
+              </h4>
+              <p class="card__paragraph">
+                For Kingdom Entrepreneurs, equipping businessmen and women with the requisite tools for doing exploits in Business.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/iLEAD.png" alt="A lady smiling with other various persons in the background" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                #iLEAD
+              </h3>
+              <h4 class="card__heading--micro">
+                International Leadership Bootcamp
+              </h4>
+              <p class="card__paragraph">
+                Global Leadership Construct. Recommended for young leaders, executives, political and business leaders.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/startup-fund-box.png" alt="Image of plants growing on a newly minted coin" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                StartUp FundBox
+              </h3>
+              <h4 class="card__heading--micro" />
+              <p class="card__paragraph">
+                At the Startup Fundbox, we’ve curated lots of startup funding
+                opportunities, Venture Capitalists, Angel Investors, SMEs
+                Grants and Loan Facilities etc.
+                The Goal is to support thousands of businesses and build
+                the next generation of Africa’s unicorns.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--rounded">
+            <img src="~/assets/img/school-of-champion.png" alt="Image of a young man with a VR headset" class="card__img">
+            <div class="card__body">
+              <h3 class="card__heading">
+                School of Champions
+              </h3>
+              <h4 class="card__heading--micro" />
+              <p class="card__paragraph">
+                The School of Champions is our unconventional
+                success school.
+                Talk about Blockchain Opporturnities, Metaverse
+                Opportunities, Affiliate Marketing, NFTs, Extended
+                Realities etc.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="impact-report" class="section section--dark">
+      <div class="row m-0">
+        <div class="col-md-6">
+          <h2 class="section__heading">
+            impact report
+          </h2>
+        </div>
+      </div>
+      <div class="row m-0">
+        <div class="col-md-4 p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              2500
+            </h3>
+            <p class="card__paragraph">
+              Over 2,500 youngsters trained on Business and Digital Monetization
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              20
+            </h3>
+            <p class="card__paragraph">
+              Young African Leaders Trained on Contemporary and Disruptive Leadership
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              60
+            </h3>
+            <p class="card__paragraph">
+              e-books, videos, are other resources shared with young africans in the last six months
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              &dollar;20,000
+            </h3>
+            <p class="card__paragraph">
+              Over $20,000 given in value at the Ci20, iLEAD and Linkedin Conference. Innovative Entrepreneurs supported.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              10,000
+            </h3>
+            <p class="card__paragraph">
+              Over 10,000 African youths connected to Linkedin for Africans Conference, providing digital solutions and opportunities
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4s p-0">
+          <div class="card--range">
+            <h3 class="card__heading">
+              100
+            </h3>
+            <p class="card__paragraph">
+              Over 100 Young Volunteers across Africa.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="photo-gallery" class="section section--light">
+      <div class="row m-0">
+        <div class="col-md-6">
+          <h2 class="section__heading">photo gallery</h2>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
 
 <script>
